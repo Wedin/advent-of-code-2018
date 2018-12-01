b3cf84a30f48d3c713691d22d2d8a1c46c75405f
Daniel Wedin <danielwedin@hotmail.se>
2018-12-01 13:12:21 +0100

    Solve part 2 of day1

--- day1/index.js
+++ day1/index.js
@@ -2,18 +2,13 @@ const fs = require('fs');
 
 function getInputArray() {
     var input = fs.readFileSync('./input.txt', 'utf8');
-    const split = input.split('\n')
-    return split;
+    return input.split('\n')
 }
 
 function parseFrequency(freq) {
-    let value;
-    if (freq.startsWith('+')) {
-        value = parseInt(freq.slice(1), 10);
-    } else {
-        value = -1 * parseInt(freq.slice(1), 10)
-    }
-    return value;
+    return freq.startsWith('+')
+        ? parseInt(freq.slice(1), 10)
+        : -parseInt(freq.slice(1), 10)
 }
 
 function puzzle() {
@@ -23,6 +18,31 @@ function puzzle() {
     }, 0)
 }
 
+function part2Puzzle() {
+    const input = getInputArray();
+    const frequencyMap = {};
+    let duplicatedFrequency = undefined;
+    let freqChange = 0;
 
-const result = puzzle();
-console.log('result: ', result);
+    while (!duplicatedFrequency) {
+        for (let index = 0; index < input.length; index++) {
+            const currFreq = parseFrequency(input[index]);
+            freqChange = freqChange + currFreq;
+
+            if (frequencyMap[freqChange]) {
+                duplicatedFrequency = freqChange;
+                break;
+            } else {
+                frequencyMap[freqChange] = true;
+            }
+        }
+    }
+
+    return duplicatedFrequency;
+}
+
+
+const resultPart1 = puzzle();
+const resultPart2 = part2Puzzle();
+console.log('result part 1: ', resultPart1);
+console.log('result part 2: ', resultPart2);
